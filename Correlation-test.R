373f84d3bf0543b7bdef6fc395347afa767b0185
Ronald Britt Jr <ronbritt@claflin.edu>
2025-09-04 16:15:26 -0400

    Add files via upload

---
+++ Correlation-test.R
@@ -0,0 +1,9 @@
+#Author: Ronald Britt Jr
+#Date: 09/04/2025
+#Purpose: Test the Correlation
+
+if(!require(devtools)) install.packages("devtools") 
+devtools::install_github("kassambara/ggpubr")
+
+#load the library ggpubr
+library(ggpubr)

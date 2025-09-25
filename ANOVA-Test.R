6a75d0091c582aa1f951998ae19e2d9e3b566a25
Ronald Britt Jr <ronbritt@claflin.edu>
2025-09-25 16:13:53 -0400

    Add files via upload

--- ANOVA-Test.R
+++ ANOVA-Test.R
@@ -1,6 +1,6 @@
 #Author: Ronald Britt Jr
 #Date: September 23rd, 2025 (09/23/2025)
-#Purpose:
+#Purpose:To test One way ANOVA
 
 #Load library dplyr)
 library(dplyr)
@@ -14,3 +14,15 @@ df
 head(df)
 
 glimpse(df)
+
+#Plot the dataset
+ggplot(df, aes(x = poison, y = time, fill = poison)) + geom_boxplot() + geom_jitter(shape = 15, color = "steelblue", position = position_jitter(0.21)) + theme_classic()
+
+anova_one_way <- aov(time~poison, data = df)
+summary(anova_one_way)
+
+ Sq Mean Sq F value   Pr(>F)    
+poison       2  1.033  0.5165   11.79 7.66e-05 ***
+Residuals   45  1.972  0.0438                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1

5c47561c7790e5e6295c0bfd605e69f354f4fd0c
Ronald Britt Jr <ronbritt@claflin.edu>
2025-11-11 15:52:55 -0500

    Add files via upload

---
+++ Chi-Square-Test.R
@@ -0,0 +1,21 @@
+#Author: Ronald Britt Jr
+#Date: 11/11/2025
+#Purpose: Test the chi square analysis
+
+#Load dummy dataset
+data_frame <- read.csv("https://goo.gl/j6lRXD")
+head(data_frame)
+
+table(data_frame$treatment, data_frame$improvement)
+
+              improved not-improved
+  not-treated       26           29
+  treated           35           15
+
+#apply chi square function to check the difference between treatment and improvement
+chisq.test(data_frame$treatment, data_frame$improvement, correct=FALSE)
+
+        Pearson's Chi-squared test
+
+data:  data_frame$treatment and data_frame$improvement
+X-squared = 5.5569, df = 1, p-value = 0.01841
